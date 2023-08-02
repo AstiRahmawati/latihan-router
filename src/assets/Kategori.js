1bd41f92904db08593c8dec80acab86b8916e4b1
WndaNugraha <wandanugraha2004@gmail.com>
2023-08-02 14:58:24 +0700

    latihan router

---
+++ src/assets/Kategori.js
@@ -0,0 +1,31 @@
+export const kategori = {
+    "kategori" : [
+        {
+            "id" : 1,
+            "nama" : "Elektronik",
+            "img" : "elektronik.jpg"
+        },
+        {
+            'id' : 2,
+            "nama" : "Perabotan Rumah Tangga",
+            "img" : "rumah tangga.jpg"
+        },
+        {
+            'id' : 3,
+            "nama" : "Aksesoris",
+            "img" : "aksesoris.webp"
+        },
+        {
+            'id' : 4,
+            "nama" : "Skincare",
+            "img" : "skincare.jpg"
+        },
+        {
+            'id' : 5,
+            "nama" : "Pakaian Wanita",
+            "img" : "pakaian.jpeg"
+        },
+        
+
+    ]
+}
---
+++ src/assets/Produk.js
@@ -0,0 +1,127 @@
+export const produk = {
+    "produk" : [
+        {
+            "id" : 1,
+            "nama" : "Handphone",
+            "harga" : "2.500.000",
+            "keterangan" : "Samsung A13 5G memiliki tiga kamera belakang yang beresolusi (50 MP, 2 MP, dan 2 MP), dan ada juga kamera depan sebesar 5 MP. ponsel seharga 2 jutaan memang umum memiliki layar dengan resolusi HD+ alias 720p. Itulah yang membuat Samsung Galaxy A13 terlihat menarik karena suguhkan resolusi Full HD+ (1080 x 2408 piksel) pada ukuran 6,6 inci. Samsung Galaxy A13 menjalankan sistem operasi (OS) Android 12 yang dipoles dengan tampilan antarmuka (UI) One UI 4.1. Fitur pendukung lainnya di ponsel ini mencakup pemindai sidik jari yang sudah dibenamkan di tombol power.",
+            "id_kategori" : 1,
+            "img" : "hp.webp"
+        },
+        {
+            'id' : 2,
+            "nama" : "Laptop",
+            "harga" : "5.000.000",
+            "keterangan" : "ASUS X441 didukung oleh prosesor hingga Intel® Core™ i3 generasi ke-8, 4GB RAM, dan grafis NVIDIA® GeForce® MX110. X Series hadir dengan preinstalled Windows 10 Home, sehingga siap untuk semua tugas komputasi sehari-hari dan menghibur Anda. X441M Series hadir dengan OS Windows 10. VivoBook Max hadir untuk kebutuhan Anda dengan telah dilengkapi USB 3.0 dan juga USB 3.1 type C yang 10x lebih cepat dari USB 3.0 dan menghemat waktu Anda untuk mencari port usb yang tepat.",
+            "id_kategori" : 1,
+            "img" : "laptop.webp"
+        },
+        {
+            'id' : 3,
+            "nama" : "Piring",
+            "harga" : "45.000",
+            "keterangan" : "Piring melamin adalah salah satu peralatan makan yang sering digunakan oleh berbagai kalangan. Harganya yang terjangkau serta bahannya yang tidak cepat rusak menjadikan melamin sebagai wadah makan andalan, dari mulai untuk keperluan rumah tangga hingga digunakan di restoran-restoran. Melamin adalah senyawa organik yang terbuat dari polimer sintetis, formaldehida, dan urea. Senyawa panas ini menciptakan resin yang bisa dibentuk untuk macam-macam peralatan rumah tangga.",
+            "id_kategori" : 2,
+            "img" : "piring.png"
+        },
+        {
+            'id' : 4,
+            "nama" : "Wajan",
+            "harga" : "100.000",
+            "keterangan" : "Wajan adalah peralatan dapur yang serbaguna. Tidak hanya untuk menggoreng, wajan penggorengan juga dapat digunakan untuk berbagai fungsi, mulai dari menumis, memanggang, hingga memasak makanan berkuah. ",
+            "id_kategori" : 2,
+            "img" : "wajan.jpeg"
+        },
+        {
+            'id' : 5,
+            "nama" : "Sapu",
+            "harga" : "25.000",
+            "keterangan": "Sapu lantai merupakan salah satu alat pembersih yang terdiri dari bagian serat atau serabut kaku dan biasanya terpasang atau terikat kepada suatu pegangan silindris. Bentuk sapu hampir selalu mengalami perubahan mulai dari bahan ranting-ranting pohon hingga seikatan serat-serat alami. ",
+            "id_kategori" : 2,
+            "img" : "sapu.jpg"
+        },
+        {
+            'id' : 6,
+            "nama" : "Komputer",
+            "harga" : "5.000.000",
+            "keterangan" : "LENOVO ThinkCentre M720T merupakan komputer desktop bertenaga yang sangat mumpuni untuk mengerjakan pekerjaan multi-tasking tanpa hambatan. Desktop ini sangat cocok untuk untuk kebutuhan kantor Anda agar karyawan Anda tetap terjaga produktivitasnya.",
+            "id_kategori" : 1,
+            "img" : "komputer.jpg"
+        },
+        {
+            'id' : 7,
+            "nama" : "Gelang",
+            "harga" : "500.000",
+            "keterangan" : "Gelang emas adalah aksesoris yang bisa digunakan pada pergelangan kaki maupun tangan. Gelang emas ini adalah salah satu aksesoris fashion yang versatile karena bisa digunakan oleh pria, wanita, maupun balita sekalipun. Gelang dapat berfungsi sebagai perhiasan atau sebagai penanda status orang yang mengenakannya. Gelang telah digunakan sepanjang sejarah oleh laki-laki dan perempuan. Digunakan baik sebagai perhiasan atau untuk menandai berbagai perbedaan di banyak kebudayaan.",
+            "id_kategori" : 3,
+            "img" : "gelang.webp"
+        },
+        {
+            'id' : 8,
+            "nama" : "Kalung",
+            "harga" : "1.000.000",
+            "keterangan" : "Titanium adalah salah satu bahan kalung yang berasal dari logam, dengan warna abu-abu putih atau perak metalik. Kalung yang cocok untuk pria ini memiliki kelebihan bersifat antikarat. Tak hanya itu, bahannya tidak memicu alergi, ringan dipakai, dan harganya masih terjangkau. alah satu karakteristik Titanium yang paling terkenal adalah dia sama kuat dengan baja tetapi hanya 60% dari berat baja. Kekuatan lelah (fatigue strength) yang lebih tinggi daripada paduan aluminium. Tahan suhu tinggi.",
+            "id_kategori" : 3,
+            "img" : "kalung.png"
+        },
+        {
+            'id' : 9,
+            "nama" : "Cincin",
+            "keterangan" : "Cincin adalah perhiasan yang melingkar di jari. Cincin dapat dipakai oleh perempuan maupun laki-laki. Secara tradisional, cincin biasanya dibuat dari logam mulia; seperti emas, perak, dan platina. Logam lainnya seperti baja antikarat, krom, besi, perunggu, kuningan, dan tembaga juga lazim digunakan. Fungsi cincin zaman dahulu sangat beragam, seperti menjadi stempel bagi para bangsawan. Selain itu, pemanah juga menggunakan cincin untuk melindungi jarinya agar tidak terluka karena terkena tali busur. O iya, cincin juga digunakan para penjahit untuk melindungi jarinya dari jarum jahit saat sedang menjahit.",
+            "harga" : "300.000",
+            "id_kategori" : 3,
+            "img" : "cincin.jpg"
+        },
+        {
+            'id' : 10,
+            "nama" : "Wardah",
+            "harga" : "500.000",
+            "keterangan" : "Wardah merupakan brand kosmetik halal asli Indonesia yang berdiri sejak 1995 di bawah PT. Paragon Technology and Innovation (PT. PTI). Didirikan oleh Nurhayati Subakat yang saat ini menjabat sebagai Komisaris PT. PTI, Wardah selalu mengedepankan kualitas untuk mendukung perempuan tampil cantik sesuai karakternya. Keunggulan dari produk Wardah dibanding dengan produk kosmetik lainnya adalah adanya jaminan halal yang disahkan oleh MUI. Obyek pada penelitian ini adalah, pelanggan kosmetik wardah di kota Jember dengan populasi dalam penelitian ini adalah pelanggan kosmetik wardah.",
+            "id_kategori" : 4,
+            "img" : "wardah.webp"
+        },
+        {
+            'id' : 11,
+            "nama" : "Camille Beauty",
+            "harga" : "400.000",
+            "keterangan" : "Berbagai jenis varian produk pada Camille Beauty ini aman digunakan oleh remaja mulai dari usia 13 tahun. Produk pada Camille Beauty ini juga aman digunakan untuk ibu hamil dan menyusui karena terbuat dari bahan alami dan sudah bersertifikat BPOM. Menyasar pembeli berusia remaja mulai dari 13 tahun, masker organik Camille Beauty memiliki berbagai macam manfaat tersendiri sehingga customer dapat menyesuaikan dengan tipe dan kondisi kulit mereka. Saat ini, Camille Beauty memiliki enam varian, yakni strawberry, lemon, green tea, coklat, milk, dan coffee.",
+            "id_kategori" : 4,
+            "img" : "camille.webp"
+        },
+        {
+            'id' : 12,
+            "nama" : "Fair & Lovely",
+            "harga" : "2500.000",
+            "keterangan" : "Fair & Lovely atau yang kini dikenal sebagai Glow & Lovely menyediakan rangkaian skincare seperti pelembab wajah yang diformulasi dengan kandungan multi vitamin. Kandungan tersebut membuat produk moisturizer dari merek ini mampu melembapkan sekaligus membuat wajah lebih cerah. ",
+            "id_kategori" : 4,
+            "img" : "fair n.jpg"
+        },
+        {
+            'id' : 13,
+            "nama" : "Tunik",
+            "harga" : "250.000",
+            "keterangan" : "Tunik adalah pakaian dengan ukuran yang lebih longgar dari model pakaian seperti biasanya sehingga mampu menutupi dada, bahu dan punggung. Pakaian ini memiliki potongan yang sangat sederhana. Pada awalnya, tunik merupakan pakaian tanpa lengan yang disebut colobium. Tunik dengan berbagai macam model, motif serta corak nyatanya cocok banget dikenakan untuk segala macam acara mulai dari kasual hingga formal, outfit untuk kuliah atau bahkan pergi ke kondangan.",
+            "id_kategori" : 5,
+            "img" : "tunik.webp"
+        },
+        {
+            'id' : 14,
+            "nama" : "Kemeja",
+            "harga" : "200.000",
+            "keterangan" : "Kemeja wanita adalah baju atau atasan yang berpola badan wanita, ada yang memiliki siluet atau kupnat sehingga terlihat dengan baik siluet atau lekuk badan pemakainya, dan memiliki kancing depan dari atas hingga bawah. ",
+            "id_kategori" : 5,
+            "img" : "kemeja3.webp"
+        },
+        {
+            'id' : 15,
+            "nama" : "Abaya",
+            "harga" : "500.000",
+            "keterangan" : "Abaya adalah sebuah busana garmen yang berasal dari timur tengah, berbentuk sederhana mirip jubah. Oleh sebab itu abaya juga diistilahkan dengan gamis arab. Usut punya usut ternyata ada kain khusus yang memang dipakai para muslimah Arab untuk merancang abaya lho. Beberapa bahan yang dimaksud diantaranya ada maliky, nida, fursan, lexus serta hareer yang lebih dikenal dengan sebutan formal jetblack kualitas premium.",
+            "id_kategori" : 5,
+            "img" : "abaya.jpg"
+        },
+        
+        
+
+    ]
+}
